af812ed887d6321a006f0f6101472ce77a0ac4a7
dragope <dragope@hotmail.com>
2022-07-22 15:16:46 -0300

    aesthetic changes

--- client/src/components/MovieMiniatureActor.jsx
+++ client/src/components/MovieMiniatureActor.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import NoPoster from '../images/no_poster.jpeg'
 import './MovieMiniature.css'
 
 function MovieMiniatureActor(movie) {
@@ -7,7 +8,7 @@ function MovieMiniatureActor(movie) {
     console.log(movie)
 
   return (
-    <div className='movie-miniature-container' style={movie.movie.poster_path !== null? { backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.movie.poster_path})`, backgroundSize: 'cover' } : { backgroundImage: `url('/client/src/images/no_poster.jpeg'})`, backgroundSize: 'cover' }}>
+    <div className='movie-miniature-container' style={movie.movie.poster_path === null ? { backgroundImage: `url(${NoPoster})`, backgroundSize: 'cover' } : { backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.movie.poster_path})`, backgroundSize: 'cover' }}>
         <div className='movie-miniature-details'>
             <p className='movie-miniature-details-title'>{ movie.movie.title } ({movie.movie.release_date.substring(0,4)})</p>
             <p className='movie-miniature-details-character'>representing {movie.movie.character}</p>
