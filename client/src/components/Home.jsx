9d174499093cc408fa3be4416d308d0af34bbb8e
dragope <dragope@hotmail.com>
2022-07-22 13:23:42 -0300

    aesthetic changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,10 +6,10 @@ function Home(){
     return(        
         <div className="home-container">
             <div className='home-claims'>
-                <h3>With WATCHLIST</h3>
+                <h3>with WATCHLIST</h3>
                 <h2>never forget to watch a movie you learned about</h2>
                 <h3>just SEARCH it and add it to your WATCHLIST</h3>
-                <h1>Start searching in the box above!</h1>
+                <h1>start searching in the box above!</h1>
             </div>
         </div>
     )
