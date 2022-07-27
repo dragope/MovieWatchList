aadcce49eee6368c3556a3d516bec8978c068fad
dragope <dragope@hotmail.com>
2022-07-27 12:30:47 -0300

    firebase security update

--- client/src/firebase/firebase-config.js
+++ client/src/firebase/firebase-config.js
@@ -11,15 +11,6 @@ export const firebaseConfig =  {
   appId: process.env.REACT_APP_APP_ID
 };
 
-// export const firebaseConfig =  {
-//   apiKey: "AIzaSyAQFrmdJfHlL0PItwa0tojuUBA0WozO2Z0",
-//   authDomain: "watchlist-bd0a2.firebaseapp.com",
-//   projectId: "watchlist-bd0a2",
-//   storageBucket: "gs://watchlist-bd0a2.appspot.com",
-//   messagingSenderId: "662576068351",
-//   appId: "1:662576068351:web:c656efaacb0845a9e83eca"
-// };
-
 export const app = initializeApp(firebaseConfig)
 export const storage = getStorage(app)
 export const auth = getAuth(app)
