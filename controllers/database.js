964c90f4aa98fa089b86499e451990f7c58287bd
dragope <dragope@hotmail.com>
2022-07-27 12:29:26 -0300

    security changes

--- controllers/database.js
+++ controllers/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+require('dotenv');
 
-mongoose.connect('mongodb://localhost:27017/watchlist')
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.suftt.mongodb.net/?retryWrites=true&w=majority`)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
